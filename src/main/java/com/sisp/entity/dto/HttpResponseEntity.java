ac5d35d0270e5df13b30b594bef73d676a5a0f2f
Luccccccccc <vivaldionly@outlook.com>
2023-06-14 14:34:34 +0800

    静态资源

--- src/main/java/com/sisp/entity/dto/HttpResponseEntity.java
+++ src/main/java/com/sisp/entity/dto/HttpResponseEntity.java
@@ -21,27 +21,4 @@ public class HttpResponseEntity{
         }
     }
 
-//    public String getCode() {
-//        return code;
-//    }
-//
-//    public void setCode(String code) {
-//        this.code = code;
-//    }
-//
-//    public Object getData() {
-//        return data;
-//    }
-//
-//    public void setData(Object data) {
-//        this.data = data;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
 }
--- src/test/java/com/sisp/ShixunApplicationTests.java
+++ src/test/java/com/sisp/ShixunApplicationTests.java
@@ -1,42 +1,69 @@
 package com.sisp;
 
 import com.sisp.Dao.UserEntityMapper;
+import com.sisp.entity.ProjectEntity;
 import com.sisp.entity.UserEntity;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Date;
+import java.util.UUID;
 
 @SpringBootTest
 class ShixunApplicationTests {
 
-    @Test
-    void contextLoads() {
+    ShixunApplicationTests() throws IOException {
     }
 
-    @Test
-    public void deleteUserByName() throws Exception {
+    private UserEntityMapper userEntityMapper = getUserEntityMapper();
+
+    private UserEntityMapper getUserEntityMapper() throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         //创建UserMapper对象，mybatis自动生成mapper代理对象
         UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
-        //调用userMapper的方法
+        return userEntityMapper;
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void insert() {
         UserEntity userEntity = new UserEntity();
-        userEntity.setUsername("aaaaa");
-        userEntity.setId("1");
-        int i = userEntityMapper.deleteUserById(userEntity);
-        if(i==0){
-            // 记录error级别的信息
-        }else{
-            System.out.println(i);
-            // 记录info级别的信息
-            System.out.println(">>delete用户删除测试成功");
-        }
+        userEntity.setId(String.valueOf(UUID.randomUUID()));
+        userEntity.setUsername("testUsername");
+        userEntity.setStartTime(new Date(new java.util.Date().getTime()));
+        userEntity.setStopTime(new Date(new java.util.Date().getTime()));
+        userEntity.setStatus("testStatus");
+        userEntity.setCreatedBy("testCreatedBy");
+        userEntity.setCreationDate(new Date(new java.util.Date().getTime()));
+        userEntity.setLastUpdatedBy("testLastUpdatedBy");
+        userEntity.setLastUpdateDate(new Date(new java.util.Date().getTime()));
+
+        Assertions.assertEquals(1, userEntityMapper.insert(userEntity));
+    }
+
+
+    @Test
+    void deleteUserById() {
+
+        UserEntity user = new UserEntity();
+        user.setId("testId");
+        userEntityMapper.insert(user);
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(user.getId());
+        Assertions.assertEquals(1, userEntityMapper.deleteUserById(userEntity));
     }
 }
